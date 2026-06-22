ee4e98b8a7ec94fb32f15f4ae64863b650bdfadb
Antonie "Anthony" Potgieter <94629441+AnthonySD07@users.noreply.github.com>
2026-06-22 13:30:15 +0200

    Landing Page updated from Raymond's branch Rating card folder and placement Updated
    Issue was the rating card was showing up on landing page but it is fixed now.
    only issue still is that the rating card is showing up in the corner and not like what it is suppose to be in the middle of the page but I will work on that next. I just wanted to get the code up and running first before I start working on the styling of the rating card.

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,32 @@
-import Image from "next/image";
-import Header from "@/components/Header";
-import Navbar from "@/components/Navbar";
-import RatingCard from "@/components/RatingCard";
-import Footer from "@/components/Footer";
+// Raymond J Tilstone
+// Design by Cione Botha
 
+import Image from "next/image";
+import Link from "next/link";
+import RegisterPage from "./Register/page";
+import LoginPage from "./login/page";
 
 export default function Home() {
-  return (   
-      <main className="min-h-screen bg-black text-white">
 
-        <Header />
-        <Navbar />
+  return (
 
-        <div className="flex justify-center pt-12">
-          <RatingCard />
-        </div>
+    // Home page parent container
+    <div className="min-h-screen flex flex-col justify-center items-center bg-[radial-gradient(circle_at_top,rgba(126,34,206,0.6)_0%,rgba(26,5,46,0.4)_35%,rgba(0,0,0,0.2)_75%)] opacity-100 ">
 
-        <Footer />        
-        
-      </main>
-  );
+      <img src="/LogiTrackLogo.png" alt="LogiTrack Logo" className="w-45 md:w-90" />
+
+      <h3 className="text-xs md:text-lg px-2 py-1 md:p-0 text-purple-500">STAY UPDATED, STAY IN CONTROL</h3>
+      <h3 className="text-xs md:text-lg px-2 py-1 md:p-0 text-pink-500 mb-5 text-center">MANAGE DELIVERIES WITH REAL-TIME TRACKING AND INSTANT UPDATES</h3>
+
+      <img src="/HomepagePic1.png" alt="Homepage Image" className="w-50 md:w-100" />
+
+      <div className="flex flex-col gap-3 p-2">
+        <button className="bg-linear-to-r from-purple-500 to-pink-500 rounded-2xl p-1 md:px-2 font-bold"><Link href="/Register">Get Started →</Link></button>
+        <button className="border border-solid border-white rounded-2xl font-bold p-1 md:px-2"><Link href="/login">Log In</Link></button>
+      </div>
+      
+    </div>
+
+  )
 
 }
